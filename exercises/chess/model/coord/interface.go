b8d976f6f3c36f073f9f89f6d251e581b74a3503
Charles <charlesdebriey@outlook.com>
2020-10-29 18:40:35 +0100

    Add non saved code

--- exercises/chess/model/coord/interface.go
+++ exercises/chess/model/coord/interface.go
@@ -1,5 +1,17 @@
+// Package coord will contain types and logic for handling chess board coordinates.
 package coord
 
-type ChessCoordinates.interface{
-	//TODO
+import "fmt"
+
+// ChessCoordinates is an interface for an abstract Coordinates.
+type ChessCoordinates interface {
+	fmt.Stringer // "A7"
+	// Coord gets n'th coordinate comp.
+	// Start with 0th coordinate.
+	// Returns an error if coordinate is inexistant.
+	Coord(n int) (int, error)
+
+	// Vector
+	// Add()
+	// Multiply()
 }
